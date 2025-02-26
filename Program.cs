ece9ea70736bb4cfbc0915bd9b448e3bad7cb307
Alexandr <86582717+tseneven@users.noreply.github.com>
2025-02-27 01:38:43 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Zadacha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(4.5);
+            arrayList.Add(18);
+
+            List<int> newCollection = new List<int> { 5, 6, 7, 8, 9 };
+
+            int n1 = int.Parse(Console.ReadLine());
+
+            arrayList.InsertRange(n1, newCollection);
+
+
+            int n = n1 / 2;
+            int k = int.Parse(Console.ReadLine());
+
+            if (n < arrayList.Count)
+            {
+                int removeCount = Math.Min(k, arrayList.Count - n);
+                arrayList.RemoveRange(n, removeCount);
+            }
+        }
+    }
+}
